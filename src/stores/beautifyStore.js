2ae3bfa507e5665961be3bb17dc097cff1da48c7
小生 <2150396701@qq.com>
2026-09-04 02:22:15 +0800

    Feature/completed beautify view (#1)
    * 完善beautify页面
    
    * 补充

--- src/stores/beautifyStore.js
+++ src/stores/beautifyStore.js
@@ -9,4 +9,37 @@ const beautifyStore = defineStore("beautify_store_date", {
   getters: {},
   actions: {}
 });
+
+function getVideoFrames(videoPath, frameCount = 3) {
+  return new Promise((resolve) => {
+    const video = document.createElement("video");
+    video.src = videoPath;
+    video.muted = true;
+    video.preload = "auto";
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const frames = [];
+
+    video.onloadeddata = () => {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      video.currentTime = 0;
+    };
+
+    video.onseeked = () => {
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      frames.push(canvas.toDataURL("image/jpeg"));
+
+      if (frames.length < frameCount) {
+        video.currentTime = (video.duration / frameCount) * frames.length;
+      } else {
+        resolve(frames);
+      }
+    };
+
+    video.play();
+  });
+}
+
 export default beautifyStore;
