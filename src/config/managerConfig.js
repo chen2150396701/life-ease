445d352768387c1870beb822502eb077c0b0cfcc
chenliangzeng <2150396701@qq.com>
2026-09-05 01:39:27 +0800

    完善beautify页面

--- src/config/managerConfig.js
+++ src/config/managerConfig.js
@@ -1,6 +1,6 @@
 const manager = {
     lift_ease_name: "清闲",
-    lift_ease_font_size:"16px",
+    lift_ease_font_size:"18px",
     background_image: "",
     background_size:"",
     background_color: "#ffffff",
